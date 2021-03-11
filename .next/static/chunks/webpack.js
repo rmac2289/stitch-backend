f425445a0f087eb68b4162023f4984bc6d6c96f3
Ross MacDonald <rmac2289@gmail.com>
2021-03-11 05:17:18 -0800

    adding user roles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "684483201f4b004846fd";
+/******/ 	var hotCurrentHash = "f6ebc3876414d6daf778";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
